446649f43bae327680062e6d5c2bbb84ec7629bc
Wojtek Boduch <wojttom.b@wp.pl>
2024-06-25 01:50:59 +0200

    a typo :3

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ function Validate(input){
 function operation(operator, input1, input2){
     switch(operator){
         case "+":
-            return input1 + input2
+            return +input1 + +input2
         case "-":
             return input1 - input2
         case "*":
@@ -68,24 +68,23 @@ function operation(operator, input1, input2){
 async function script(){
     let CheckSum = 0
     let sum 
-    let input1 = parseInt(document.forms["Form"]["num1"].value);
-    let input2 = parseInt(document.forms["Form"]["num2"].value);
+    let input1 = document.forms["Form"]["num1"].value;
+    let input2 = document.forms["Form"]["num2"].value;
     let operator = document.forms["Form"]["operation"].value
     let check1 = Validate(input1)
     if(check1){
         let check2 = Validate(input2)
         CheckSum = (check1 + check2);
     }
-    if(CheckSum=== 2){
-        log(operator)
+    if(CheckSum===2){
         sum = operation(operator,input1,input2)
         log(sum)
-        // loading.style.display="visible"
-        // loading.style.visibility="visible"
-        // draw(sum)
-        // audio.play()
-        // await wait(6300)
-        // loading.style.display="none"
+        loading.style.display="visible"
+        loading.style.visibility="visible"
+        draw(sum)
+        audio.play()
+        await wait(6300)
+        loading.style.display="none"
         c.style.visibility="visible"
         c.style.animation="crop 4s linear"
     }
